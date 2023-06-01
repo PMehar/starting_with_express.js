2dc1e8448eb43e1cb71942d7004610253e28ffd7
PMehar <pujamehar05@gmail.com>
2023-06-01 18:14:30 +0530

    parsing incoming requests and extracting data

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express'); //express package
+const bodyParser = require('body-parser');
 
 const app = express();//running express as a function
-const value = "book";
 
-app.use((req,res,next)=>{
-    console.log('In the middleware!');
-    next(); //to allow the req to go on to the next middleware in line 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product"method="POST"><input type="text" name="title" placeholder="Add product" required><input type="number" name="size" placeholder= "Add size"><button type ="submit" required>Add Product</button></input></form>');
 });
-app.use((req,res,next)=>{
-    console.log('In another middleware!');
-    //res.send('<h1>Hello From Express!</h1>');
-    res.send( { key1: value }) 
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello From Express!</h1>');
 });
 
 app.listen(3000);
