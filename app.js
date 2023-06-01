82d48a88030ff9eac528684dafa7a81f2577f3a5
PMehar <pujamehar05@gmail.com>
2023-06-01 15:38:53 +0530

    started express

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const express = require('express'); //express package
+
+const app = express();//running express as a function
+const value = "book";
+
+app.use((req,res,next)=>{
+    console.log('In the middleware!');
+    next(); //to allow the req to go on to the next middleware in line 
+});
+app.use((req,res,next)=>{
+    console.log('In another middleware!');
+    //res.send('<h1>Hello From Express!</h1>');
+    res.send( { key1: value }) 
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
