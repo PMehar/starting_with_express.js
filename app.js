a1d74325973ac5be40e3c5da90ebc0e958f98e20
PMehar <pujamehar05@gmail.com>
2023-06-02 22:19:00 +0530

    cleaned code using routes and filter

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();//running express as a function
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product"method="POST"><input type="text" name="title" placeholder="Add product" required><input type="number" name="size" placeholder= "Add size"><button type ="submit" required>Add Product</button></input></form>');
-});
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello From Express!</h1>');
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
